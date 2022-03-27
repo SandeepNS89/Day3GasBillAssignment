896e9b6a013793bc8b1c026a2a1aac77cafe846c
SandeepNS89 <102049826+SandeepNS89@users.noreply.github.com>
2022-03-27 14:48:36 +0530

    Add files via upload

--- AssociationTest.java
+++ AssociationTest.java
@@ -1,34 +1,34 @@
+//import javax.swing.border.LineBorder;
 
 public class AssociationTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Kfc kfcObj = new Kfc();
-		
+		Kfc kfcObj = new Kfc("Hygine");
+		kfcObj.maintain();
 		//System.out.println(kfcObj);
 		Branch br = new Branch();
-		kfcObj.account(br);
+		kfcObj.billPayment(br);
+		kfcObj.consumptionBill(br);
 		
-		Trading td = new Trading();
-		kfcObj.payment(td);
+//		Trading td = new Trading();
+//		kfcObj.payment(td);
 		
 		TwoWheeler tw = new TwoWheeler();
 		kfcObj.delivery(tw);
 		
-		GasBill gb = new GasBill();
-		kfcObj.consumptionBill(gb);
+//		GasBill gb = new GasBill();
+//		kfcObj.consumptionBill(gb);
 		
-		Library lb = new Library();
-		kfcObj.study(lb);
+		Library lib = new Library();
+		kfcObj.libraryContains(lib);
 		
-		Book bk = new Book();
-		kfcObj.wrote(bk);
+		ClassRoom cs = new ClassRoom();
+		Kfc.entertainCustumer(cs);
 		
 		ClassRoom cr = new ClassRoom();
 		kfcObj.training(cr);
-		
-		Guiter g = new Guiter();
-		kfcObj.entertain(g);
+	
 		
 		Food fd = kfcObj.serveFood();		
 		FeedBack fb = fd.customerResponse();
@@ -38,27 +38,36 @@ public class AssociationTest {
 
 class Company
 {
-	public static void tagLine()
+	Company()
 	{
+		System.out.println("------constructor ----------------");
 		System.out.println("TagLine--Good Food For Good Health");
+		System.out.println("----------------------------------");
+		
 	}
 }
 
-class Resturant extends Company
+abstract class Resturant extends Company
 {
-	
+	abstract void maintain();
+	Resturant(String hy)
+	{
+		System.out.println("----constructor with arguments -----");
+		System.out.println("This is Resturant is Hegenic : " + hy);
+		
+	}
 }
 
-class AirConditioner
+  class AirConditioner
 {
-	
+	//void facility(;)
 }
 
 class Shirt
 {
 	Shirt()
 	{
-		System.out.println("Shirt : Unique dresscode");
+		//System.out.println("Shirt : Unique dresscode");
 	}
 }
 
@@ -66,7 +75,7 @@ class Aquirium
 {
 	Aquirium()
 	{
-	System.out.println("Aquirium : For decoration");
+	//System.out.println("Aquirium : For decoration");
 	}
 }
 
@@ -82,6 +91,12 @@ class Toys
 
 final class Kfc extends Resturant
 {
+	Kfc(String hy) {
+		super(hy);
+		// TODO Auto-generated constructor stub
+		System.out.println("-----super ctor-------");
+		System.out.println("-------------------------------------");
+	}
 	int branchNumber;
 	String location;
 
@@ -91,37 +106,54 @@ final class Kfc extends Resturant
 	DisplayScreen dsObj = new DisplayScreen();
 	Toys toyObj = new Toys();
 	
+
+	void maintain()
+	{
+		System.out.println("------Abstruct Class-----");
+		System.out.println("Well maintained");
+		System.out.println("-----------------------------");
+	}
 	
-	void account(Branch b)
-	{
-		System.out.println("Maintains Account in Bank" + b);
-	}
-	void consumptionBill(GasBill gb)
-	{
-		System.out.println("Bill for consuming gas" + gb);
-	}
+	
 	void delivery(TwoWheeler tw)
 	{
-		System.out.println("Uses two wheeler to deliver food" + tw);
+		//System.out.println("Uses two wheeler to deliver food" + tw);
     }
-	void payment(Trading td)
-	{
-		System.out.println("Both online & offline trading/billing acceptable" + td);
+	
 
-	}
-	void study(Library lb)
+	void libraryContains(Book bk)
 	{
-		System.out.println("Chef uses library to study HM" + lb);
+		System.out.println("--- Default Interface and Object Slice---------");
+		System.out.println("Library has many books");
+		bk.compitativeBook();
+		bk.storyBook();
+		bk.novelBook();
+		System.out.println("-----------------------------");
 	}
-	void wrote(Book bk)
+	static void entertainCustumer(Guiter g)
 	{
-		System.out.println("Chef wrote many books" + bk);
-
+		System.out.println("-----Static,Interface and Object Slice-------");
+		System.out.println("Guiterist are there to entertain customers");
+		g.guiterBasicsTraining();
+		g.guitorInterTraining();
+		g.guitorAdvanceTraining();
+		System.out.println("-----------------------------");
 	}
-	void entertain(Guiter g)
+	
+	protected void billPayment(Trading td)
 	{
-		System.out.println("Guiterist are there to entertain customers" + g);
-
+		System.out.println("---Protected,MultipleInterface and ObjectSlice-------");
+		System.out.println("Bill payments");
+		td.onlineBill();
+		td.offlineBill();
+		System.out.println("-----------------------------");	
+	}
+	public void consumptionBill(GasBill gb)
+	{
+		System.out.println("---Public,MultipleInterface and ObjectSlice-------");
+		System.out.println("Bill for consuming gas");
+		gb.gasBilldue();
+		System.out.println("-----------------------------");
 	}
 	void training(ClassRoom cr)
 	{
@@ -136,14 +168,10 @@ final class Kfc extends Resturant
 	
 }
 
-class Branch
-{
-	
-}
 
-class GasBill
+interface GasBill
 {
-	
+	void gasBilldue();
 }
 
 class TwoWheeler
@@ -151,26 +179,101 @@ class TwoWheeler
 	
 }
 
-class Trading
+interface Trading
 {
+	void onlineBill();
+	void offlineBill();
+}
+class Branch implements Trading,GasBill
+{
+
+	@Override
+	public void onlineBill() {
+		// TODO Auto-generated method stub
+		System.out.println("you pay bill through online mode");
+	}
+
+	@Override
+	public void offlineBill() {
+		// TODO Auto-generated method stub
+		System.out.println("you pay bill through offline mode");
+	}
+
+	@Override
+	public void gasBilldue() {
+		// TODO Auto-generated method stub
+		System.out.println("pay your gas bill");
+	}
 	
 }
 
-class Library
+interface Book
 {
+	void chefBook();
+	void storyBook();
+	void compitativeBook();
+	void novelBook();
+	//void guitorLearningBook();
 	
 }
 
-class Book
+class Library implements Book
 {
+	@Override
+	public void storyBook() {
+		// TODO Auto-generated method stub
+		System.out.println("Library has many story books");
+	}
+
+	@Override
+	public void compitativeBook() {
+		// TODO Auto-generated method stub
+		System.out.println("Library has many compitative books");
+	}
+
+	@Override
+	public void novelBook() {
+		// TODO Auto-generated method stub
+		System.out.println("Library has many novel books");
+	}
+
+	@Override
+	public void chefBook() {
+		// TODO Auto-generated method stub
+		System.out.println("Library has many chef books");
+	}
 	
 }
-class Guiter
+
+
+interface Guiter
 {
-	
+	void guiterBasicsTraining();
+	void guitorInterTraining();
+	void guitorAdvanceTraining();
 }
-class ClassRoom
+class ClassRoom implements Guiter
 {
+	@Override
+	public void guiterBasicsTraining() {
+		// TODO Auto-generated method stub
+		System.out.println("Classroom has many guitor basic training classes");
+
+	}
+
+	@Override
+	public void guitorInterTraining() {
+		// TODO Auto-generated method stub
+		System.out.println("Classroom has many guitor intermidiate training classes");
+
+	}
+
+	@Override
+	public void guitorAdvanceTraining() {
+		// TODO Auto-generated method stub
+		System.out.println("Classroom has many guitor advance training classes");
+
+	}
 	
 }
 class Food
