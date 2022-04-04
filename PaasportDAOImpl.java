e55f9fce429ee520b019dc337b82e42e9bd1296d
SandeepNS89 <102049826+SandeepNS89@users.noreply.github.com>
2022-04-05 00:41:47 +0530

    Add files via upload

---
+++ PaasportDAOImpl.java
@@ -0,0 +1,31 @@
+import java.util.Set;
+
+public class PaasportDAOImpl implements PassportDAO 
+{
+
+	public void addPassport() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void modifyPassport() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void deletePassport() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Employee findPassport() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Set<Employee> findAllPassport() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
